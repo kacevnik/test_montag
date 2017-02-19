d298323c66f4802fe216c47ff8f42753b44e64ac
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-02-19 12:54:47 +0300

    Отработка появления окна с инфо

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ jQuery(function($) {
 	$('.fon_activ').height($(document).height());
 
 	$('input[name="phone"]').mask("+7 (999) 999-9999");
-	
+
 	$('.li_click').click(function(){
 		var grup = $(this).attr('grup');
 		$('.li_click[grup="'+grup+'"]').removeClass('li_check');
@@ -11,12 +11,15 @@ jQuery(function($) {
 			$(this).addClass('li_check');
 		}
 		$(this).parent('ul').next().attr('check', '1');
+		$('.result span').text(grup*2000+'р');
+		$('.informa').slideDown('slow');
 	});
 
 	$('#start').click(function(){
 		$('.fon').height($(document).height());
 		$('.fon').addClass('fon_activ');
 		$('.enter').hide();
+		$('.for_form').show();
 		$('.test').fadeIn(300);	
 		return false;	
 	});
